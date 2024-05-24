432f0e410544c63579b6b9763e657b5391e78096
sam-lo <54739761+sam-lo@users.noreply.github.com>
2024-05-25 02:16:37 +0800

    support page rewrite

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 export default function Question() {
   return (
     <>
-      <Session title="關於手笛的常見問題" description="香港手笛協會Hong Kong Handflute Association 自從2021年起於香港本地推廣手笛音樂文化，以非牟利形式與不同機構合作，舉辦手笛工作坊、體驗講座、商業演出、非牟利項目等等。希望令更多人認識手笛，這個獨特的音樂形式。">
-      asd
+      <Session title="關於手笛的常見問題" description="">
+        {""}
       </Session>
     </>
   )
